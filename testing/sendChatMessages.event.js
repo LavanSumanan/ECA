e826ca008725ec84d26c3135bef41e0e2c13ea38
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-12-21 14:25:05 -0500

    using helper functions in testing code

--- testing/sendChatMessages.event.js
+++ testing/sendChatMessages.event.js
@@ -1,26 +1,15 @@
-function sendMessageToDevServer(client, inputChannel, message) {
-  client.channels.cache
-    .find((channel) => channel.name === inputChannel)
-    .send(message);
-}
-
-function sendMessageToProdServer(client, inputChannel, message) {
-  client.guilds.cache
-    .get("572591630503378945")
-    .channels.cache.find((channel) => channel.name === inputChannel)
-    .send(message);
-}
-
-function dmUser(client, inputUser, message) {
-  client.users.cache.get(inputUser).send(message);
-}
+require("dotenv").config();
+const { sendMessageToServer, dmUser } = require("../helpers/message");
 
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    // sendMessageToDevServer(client, "general", "test");
-    // sendMessageToProdServer(client, "bot-stuffs", "test");
+    // -----------------------------------ECA Test Server----------------------------------
+    // sendMessageToServer(client, "general", "test", process.env.GUILD_ID);
+    // -------------------------------------ACE Server-------------------------------------
+    // sendMessageToServer(client, "bot-stuffs", "", process.env.PROD_ID);
+    // ---------------------------------------User DM--------------------------------------
     // dmUser(client, "280871651065856001", "test123");
   },
 };
