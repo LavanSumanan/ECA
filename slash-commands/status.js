8e520d07483a99cc26295287f8581aa0662c0c0e
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-01-06 15:41:10 -0500

    updated status feature

---
+++ slash-commands/status.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const statusSchema = require("../Schemas/status-schema");
+
+module.exports = {
+  level: "mod",
+  data: new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("Set ECA's status!")
+    .addStringOption((option) =>
+      option
+        .setName("name")
+        .setDescription("What do you want ECA to listen to?")
+        .setRequired(true)
+    )
+    .setDefaultPermission(false),
+  async execute(interaction, client) {
+    // Get options from interaction
+    const options = interaction.options;
+
+    console.log("status changed by", interaction.member.user.username);
+
+    if (!interaction.member.permissions.any("ADMINISTRATOR")) {
+      interaction.reply({
+        content: `Sorry! Only executives can run this command!`,
+        ephemeral: true,
+      });
+    }
+
+    const name = options.get("name").value;
+
+    await statusSchema.deleteMany({});
+
+    try {
+      await new statusSchema({
+        name: name,
+        type: "LISTENING",
+      }).save();
+    } catch (e) {
+      console.error(e);
+    }
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: name,
+          type: "LISTENING",
+        },
+      ],
+      status: "online",
+    });
+
+    interaction.reply({
+      content: `Got it! ECA is now listening to ${name}`,
+      ephemeral: true,
+    });
+  },
+};
