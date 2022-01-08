67cdbbcafdef9a2fbebe2241b59333bdb79c1024
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-01-08 17:46:15 -0500

    created helper to edit messages

--- helpers/message.js
+++ helpers/message.js
@@ -1,21 +1,50 @@
 function sendMessageToServer(client, inputChannel, message, guildID) {
   if (guildID) {
-    client.guilds.cache
+    return client.guilds.cache
       .get(guildID)
       .channels.cache.find((channel) => channel.name === inputChannel)
       .send(message);
-    return;
+  } else {
+    return client.channels.cache
+      .find((channel) => channel.name === inputChannel)
+      .send(message);
   }
-  client.channels.cache
-    .find((channel) => channel.name === inputChannel)
-    .send(message);
 }
 
 function dmUser(client, inputUser, message) {
   client.users.cache.get(inputUser).send(message);
 }
 
+async function editMessageById(
+  client,
+  inputChannel,
+  messageId,
+  newMessage,
+  toAppend
+) {
+  let message;
+  try {
+    message = await client.channels.cache
+      .find((channel) => channel.name === inputChannel)
+      .messages.fetch(messageId);
+
+    let finalMessage;
+    if (toAppend) {
+      finalMessage = message.content + "\n" + newMessage;
+    } else {
+      finalMessage = newMessage;
+    }
+
+    await message.edit(finalMessage);
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
 module.exports = {
   sendMessageToServer: sendMessageToServer,
   dmUser: dmUser,
+  editMessageById: editMessageById,
 };
