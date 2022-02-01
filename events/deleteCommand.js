1ce9b6e57dcf9209534587f62210bfb80d64bdfa
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-01-31 22:05:17 -0500

    remove test commands from guild commands list

---
+++ events/deleteCommand.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const fs = require("fs");
+
+module.exports = {
+  name: "ready",
+  once: true,
+  async execute(client) {
+    // get all commands from this guild
+    if (!client.application?.owner) await client.application?.fetch();
+    const commands = await client.guilds.cache
+      .get(process.env.PROD_ID)
+      ?.commands.fetch();
+
+    const re = /test/;
+
+    commands.forEach((command) => {
+      //   console.log(command.name, " ", command.id);
+      if (re.test(command.name))
+        try {
+          command.delete();
+        } catch (e) {
+          console.log(e);
+        }
+    });
+  },
+};
