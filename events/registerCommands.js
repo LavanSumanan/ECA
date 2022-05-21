9f8e989bb729b895d94ecbc6dc928fc4e6fe61f3
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-05-20 22:28:13 -0400

    log registered commands

--- events/registerCommands.js
+++ events/registerCommands.js
@@ -46,13 +46,15 @@ module.exports = {
           await rest.put(Routes.applicationCommands(CLIENT_ID), {
             body: slashCommands,
           });
-          console.log("Successfully registered application commands globally");
+          console.log(
+            `Successfully registered ${slashCommands.length} application commands globally`
+          );
         } else {
           await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
             body: slashCommands,
           });
           console.log(
-            "Successfully registered application commands for development guild"
+            `Successfully registered ${slashCommands.length} application commands for development guild`
           );
         }
       } catch (error) {
