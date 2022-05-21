995294c65c7b079d54d80306acb943f6bfc5a445
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-05-20 22:04:56 -0400

    fix

--- helpers/message.js
+++ helpers/message.js
@@ -1,5 +1,3 @@
-const appreciateEmbed = require("../embeds/appreciate-embed");
-
 function sendMessageToServer(client, inputChannel, message, guildID) {
   if (guildID) {
     if (![0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(parseInt(inputChannel[0])))
