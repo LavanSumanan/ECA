6649c4cca0ebffa268cf92ea3601f2c0ffd02ccf
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-07-27 13:05:34 -0400

    feat: create seed file for test database

---
+++ seed.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const birthday = require("./Schemas/birthday");
+require("dotenv").config();
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_TEST_URI);
+    console.log("[DB Connection] Seed file");
+  } catch (err) {
+    console.log("[ERROR] Failed to connect to DB: ", err);
+    return;
+  }
+};
+
+const seedBirthdays = [
+  {
+    userid: "280871651065856001",
+    month: 11,
+    day: 6,
+  },
+  {
+    userid: "970709253906112572",
+    month: 7,
+    day: 30,
+  },
+  {
+    userid: "1",
+    month: 1,
+    day: 1,
+  },
+  {
+    userid: "2",
+    month: 2,
+    day: 2,
+  },
+];
+
+(async () => {
+  await connectToDb();
+  await birthday.deleteMany({});
+  await birthday.insertMany(seedBirthdays);
+  await mongoose.connection.close();
+})();
